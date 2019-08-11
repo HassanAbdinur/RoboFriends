18db678ad19740336f1a60f156c12fe0e246de21
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-11 00:05:00 -0700

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CardList from './CardList';
-import searchBox from './searchBox';
+import SearchBox from './SearchBox';
 import { robots } from './robots';
 
 const App = () => {
