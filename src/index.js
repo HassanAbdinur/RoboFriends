948d2a9e9c26c9db9608751bccb9807549df357d
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-13 15:57:19 -0700

    passing down store to the provider component

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ import 'tachyons';
 const store = createStore(searchRobots)
 
 ReactDOM.render(
-                <Provider>
-                    <App store={store} />
+                <Provider store={store}>
+                    <App />
                 </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
