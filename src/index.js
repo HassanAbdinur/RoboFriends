06e7a0eae246c7bf2ff8061a481ad7fc602bea8a
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-13 01:15:27 -0700

    adding changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
+const store = createStore(rootReducer)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
