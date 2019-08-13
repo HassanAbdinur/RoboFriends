87472391badc90c4083db7416aae4e4376b77410
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-13 00:37:16 -0700

    making changes

--- src/reducers.js
+++ src/reducers.js
@@ -1,9 +1,12 @@
+import { CHANGE_SEARCH_FIELD } from './constants.js';
+
 const intialState = {
     searchField: ''
 }
 
 export const searchRobots = (state=intialState, action={}) => {
     switch(action.type) {
-        
+        case: CHANGE_SEARCH_FIELD:
+        return
     }
 }
