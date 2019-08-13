91493f102aad9504ef68df887b6932f68a8ba44d
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-13 00:51:58 -0700

    updating new feature

--- src/reducers.js
+++ src/reducers.js
@@ -7,6 +7,8 @@ const intialState = {
 export const searchRobots = (state=intialState, action={}) => {
     switch(action.type) {
         case: CHANGE_SEARCH_FIELD:
-        return
+            return Object.assign({}, state, searchField: action.payload);
+        default:
+            return state;
     }
 }
