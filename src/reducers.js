20090e7eebbbc6cc0c52b50f78142d3f0071de92
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-14 18:37:32 -0700

    adding new reducer for requestRobots

--- src/reducers.js
+++ src/reducers.js
@@ -5,11 +5,11 @@ import {
     REQUEST_ROBOTS_FAILED
 } from './constants.js';
 
-const intialState = {
+const intialStateSearch = {
     searchField: ''
 }
 
-export const searchRobots = (state=intialState, action={}) => {
+export const searchRobots = (state=intialStateSearch, action={}) => {
     switch(action.type) {
         case CHANGE_SEARCH_FIELD:
             return Object.assign({}, state, { searchField: action.payload });
@@ -18,10 +18,21 @@ export const searchRobots = (state=intialState, action={}) => {
     }
 }
 
-export const requestRobots = (state=intialState, action={}) => {
+const intialStateRobots = {
+    isPending: false,
+    robots: [],
+    error: ''
+}
+
+export const requestRobots = (state=intialStateRobots, action={}) => {
     switch(action.type) {
         case REQUEST_ROBOTS_PENDING:
             return Object.assign({}, state, { isPending: true })
-            
+        case REQUEST_ROBOTS_SUCCESS:
+            return Object.assign({}, state, { robots: action.payload, isPending: false })
+        case REQUEST_ROBOTS_FAILED:
+            return Object.assign({}, state, { error: action.payload, isPending: false })
+        default:
+            return state;
     }
 }
